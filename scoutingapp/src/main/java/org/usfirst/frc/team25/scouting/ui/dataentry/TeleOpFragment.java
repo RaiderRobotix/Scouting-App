6360dc43631c28c8b2574e2fcb0429e85a1d7dab
hybras <24651269+hybras@users.noreply.github.com>
2020-02-05 14:53:14 -0500

    Refactor TeleOpFragment to use immutable scoutentry

--- scoutingapp/src/main/java/org/usfirst/frc/team25/scouting/ui/dataentry/TeleOpFragment.java
+++ scoutingapp/src/main/java/org/usfirst/frc/team25/scouting/ui/dataentry/TeleOpFragment.java
@@ -16,6 +16,8 @@ import org.usfirst.frc.team25.scouting.data.models.TeleOp;
 import org.usfirst.frc.team25.scouting.ui.UiHelper;
 import org.usfirst.frc.team25.scouting.ui.views.ButtonIncDecView;
 
+import lombok.val;
+
 import static org.usfirst.frc.team25.scouting.ui.UiHelper.hideKeyboard;
 
 
@@ -117,47 +119,51 @@ public class TeleOpFragment extends Fragment implements EntryFragment {
     }
     @Override
     public void autoPopulate() {
-        if (entry.getTeleOp() != null) {
-            TeleOp tele = entry.getTeleOp();
+        entry.getTeleOp();
+        TeleOp tele = entry.getTeleOp();
 
-            cellsScoredOuter.setValue(tele.getCellsScoredOuter());
-            cellsScoredInner.setValue(tele.getCellsScoredInner());
-            cellsScoredBottom.setValue(tele.getCellsScoredBottom());
-            cellsDropped.setValue(tele.getCellsDropped());
-            climbAssistedByPartners.setValue(tele.getAssistedClimbs());
-            rungLevel.setChecked(tele.getRungLevel());
-            attemptedClimb.setChecked(tele.getAttemptHang());
-            successClimb.setChecked(tele.getSuccessHang());
-            rotationControl.setChecked(tele.getRotationControl());
-            rotationOverspun.setChecked(tele.getRotationOverspun());
+        cellsScoredOuter.setValue(tele.getCellsScoredOuter());
+        cellsScoredInner.setValue(tele.getCellsScoredInner());
+        cellsScoredBottom.setValue(tele.getCellsScoredBottom());
+        cellsDropped.setValue(tele.getCellsDropped());
+        climbAssistedByPartners.setValue(tele.getAssistedClimbs());
+        rungLevel.setChecked(tele.getRungLevel());
+        attemptedClimb.setChecked(tele.getAttemptHang());
+        successClimb.setChecked(tele.getSuccessHang());
+        rotationControl.setChecked(tele.getRotationControl());
+        rotationOverspun.setChecked(tele.getRotationOverspun());
 
 
-            if (tele.getAssistingClimbTeamNum() != 0) {
-                assistingClimbTeamNum.setText(Integer.toString(tele.getAssistingClimbTeamNum()));
-            }
-
+        if (tele.getAssistingClimbTeamNum() != 0) {
+            assistingClimbTeamNum.setText(Integer.toString(tele.getAssistingClimbTeamNum()));
         }
+
     }
 
     @Override
     public void saveState() {
-
-        entry.setTeleOp(new TeleOp(
-                        cellsScoredBottom.getValue(),
-                        cellsScoredInner.getValue(),
-                        cellsScoredOuter.getValue(),
-                        cellsDropped.getValue(),
-                        rotationControl.isChecked(),
-                        rotationOverspun.isChecked(),
-                        positionControl.isChecked(),
-                        attemptedClimb.isChecked(),
-                        successClimb.isChecked(),
-                        rungLevel.isChecked(),
-                        climbAssistedByPartners.getValue(),
-                        UiHelper.getIntegerFromTextBox(assistingClimbTeamNum)
+        val entry = getEntry();
+        setEntry(
+                new ScoutEntry(
+                        entry.getPreMatch(),
+                        entry.getAutonomous(),
+                        new TeleOp(
+                                cellsScoredBottom.getValue(),
+                                cellsScoredInner.getValue(),
+                                cellsScoredOuter.getValue(),
+                                cellsDropped.getValue(),
+                                rotationControl.isChecked(),
+                                rotationOverspun.isChecked(),
+                                positionControl.isChecked(),
+                                attemptedClimb.isChecked(),
+                                successClimb.isChecked(),
+                                rungLevel.isChecked(),
+                                climbAssistedByPartners.getValue(),
+                                UiHelper.getIntegerFromTextBox(assistingClimbTeamNum)
+                        ),
+                        entry.getPostMatch()
                 )
         );
-
     }
 
 
