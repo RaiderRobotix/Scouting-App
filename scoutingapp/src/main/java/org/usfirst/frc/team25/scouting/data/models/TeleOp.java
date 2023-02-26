8e32b2b2fe5ca9bade33b13638ce25b64c6e46bc
Kavinraj Jayaraj <kavinraj.jayaraj@gmail.com>
2023-02-25 21:42:47 -0500

    Scouting App Completed just need to fix the file saving issue

--- scoutingapp/src/main/java/org/usfirst/frc/team25/scouting/data/models/TeleOp.java
+++ scoutingapp/src/main/java/org/usfirst/frc/team25/scouting/data/models/TeleOp.java
@@ -30,16 +30,27 @@ public class TeleOp {
     private boolean robotCommitedFoulTele;
     private String foulTypeTele;
 
-    //private HashMap<CheckBox,Boolean> gridVals;
-    private ArrayList<Boolean> gridVals;
+    private boolean topRow_leftComm_LeftCone,topRow_leftComm_MidCube, topRow_leftComm_RightCone,topRow_midComm_LeftCone,topRow_midComm_MidCube, topRow_midComm_RightCone,
+            topRow_rightComm_LeftCone,topRow_rightComm_MidCube, topRow_rightComm_RightCone,midRow_leftComm_LeftCone,midRow_leftComm_MidCube, midRow_leftComm_RightCone,
+            midRow_midComm_LeftCone,midRow_midComm_MidCube, midRow_midComm_RightCone, midRow_rightComm_LeftCone,midRow_rightComm_MidCube, midRow_rightComm_RightCone,
+            bttmRow_leftComm_LeftCone,bttmRow_leftComm_MidCube, bttmRow_leftComm_RightCone,bttmRow_midComm_LeftCone,bttmRow_midComm_MidCube, bttmRow_midComm_RightCone,
+            bttmRow_rightComm_LeftCone,bttmRow_rightComm_MidCube, bttmRow_rightComm_RightCone;
 
-    public TeleOp(int coneTop, int cubeTop, int coneMid,
-                  int cubeMid, int coneBttm, int cubeBttm,
-                  int coneDropped, int cubeDropped,
-                  boolean dockAttempt, String dockStatus,
-                  boolean robotCommitedFoul, String foulType
-                  //HashMap<CheckBox,Boolean> gridVals
-                  //ArrayList<Boolean> gridVals
+    private HashMap<CheckBox,Boolean> gridVals;
+    //private ArrayList<Boolean> gridVals;
+
+    public TeleOp(int coneTop, int cubeTop, int coneMid, int cubeMid, int coneBttm, int cubeBttm, int coneDropped,
+                  int cubeDropped, boolean dockAttempt, String dockStatus, boolean robotCommitedFoul, String foulType
+                  ,boolean topRow_leftComm_LeftCone, boolean topRow_leftComm_MidCube, boolean topRow_leftComm_RightCone
+            ,boolean topRow_midComm_LeftCone, boolean topRow_midComm_MidCube, boolean topRow_midComm_RightCone
+            ,boolean topRow_rightComm_LeftCone, boolean topRow_rightComm_MidCube, boolean topRow_rightComm_RightCone
+                  ,boolean midRow_leftComm_LeftCone, boolean midRow_leftComm_MidCube, boolean midRow_leftComm_RightCone
+            ,boolean midRow_midComm_LeftCone, boolean midRow_midComm_MidCube, boolean midRow_midComm_RightCone
+            ,boolean midRow_rightComm_LeftCone, boolean midRow_rightComm_MidCube, boolean midRow_rightComm_RightCone
+            ,boolean bttmRow_leftComm_LeftCone, boolean bttmRow_leftComm_MidCube, boolean bttmRow_leftComm_RightCone
+            ,boolean bttmRow_midComm_LeftCone, boolean bttmRow_midComm_MidCube, boolean bttmRow_midComm_RightCone
+            ,boolean bttmRow_rightComm_LeftCone, boolean bttmRow_rightComm_MidCube, boolean bttmRow_rightComm_RightCone
+
     ) {
         this.coneTopTele = coneTop;
         this.cubeTopTele = cubeTop;
@@ -53,8 +64,39 @@ public class TeleOp {
         this.dockStatusTele = dockStatus;
         this.robotCommitedFoulTele = robotCommitedFoul;
         this.foulTypeTele = foulType;
-        //this.gridVals = gridVals;
+//        this.gridVals = gridVals;
+        this.topRow_leftComm_LeftCone = topRow_leftComm_LeftCone;
+        this.topRow_leftComm_MidCube = topRow_leftComm_MidCube;
+        this.topRow_leftComm_RightCone = topRow_leftComm_RightCone;
+        this.topRow_midComm_LeftCone = topRow_midComm_LeftCone;
+        this.topRow_midComm_MidCube = topRow_midComm_MidCube;
+        this.topRow_midComm_RightCone = topRow_midComm_RightCone;
+        this.topRow_rightComm_LeftCone = topRow_rightComm_LeftCone;
+        this.topRow_rightComm_MidCube = topRow_rightComm_MidCube;
+        this.topRow_rightComm_RightCone = topRow_rightComm_RightCone;
+
+        this.midRow_leftComm_LeftCone = midRow_leftComm_LeftCone;
+        this.midRow_leftComm_MidCube = midRow_leftComm_MidCube;
+        this.midRow_leftComm_RightCone = midRow_leftComm_RightCone;
+        this.midRow_midComm_LeftCone = midRow_midComm_LeftCone;
+        this.midRow_midComm_MidCube = midRow_midComm_MidCube;
+        this.midRow_midComm_RightCone = midRow_midComm_RightCone;
+        this.midRow_rightComm_LeftCone = midRow_rightComm_LeftCone;
+        this.midRow_rightComm_MidCube = midRow_rightComm_MidCube;
+        this.midRow_rightComm_RightCone = midRow_rightComm_RightCone;
+
+        this.bttmRow_leftComm_LeftCone = bttmRow_leftComm_LeftCone;
+        this.bttmRow_leftComm_MidCube = bttmRow_leftComm_MidCube;
+        this.bttmRow_leftComm_RightCone = bttmRow_leftComm_RightCone;
+        this.bttmRow_midComm_LeftCone = bttmRow_midComm_LeftCone;
+        this.bttmRow_midComm_MidCube = bttmRow_midComm_MidCube;
+        this.bttmRow_midComm_RightCone = bttmRow_midComm_RightCone;
+        this.bttmRow_rightComm_LeftCone = bttmRow_rightComm_LeftCone;
+        this.bttmRow_rightComm_MidCube = bttmRow_rightComm_MidCube;
+        this.bttmRow_rightComm_RightCone = bttmRow_rightComm_RightCone;
     }
+
+
 
 
     public boolean isDockAttempt() {
@@ -105,11 +147,116 @@ public class TeleOp {
         return foulTypeTele;
     }
 
-//    public HashMap<CheckBox,Boolean> getGridVals() {
-//    return gridVals; }
+    //public HashMap<CheckBox,Boolean> getGridVals() { return gridVals; }
+
+    public boolean getBttmRow_leftComm_LeftCone() {
+        return bttmRow_leftComm_LeftCone;
+    }
+
+    public boolean getBttmRow_leftComm_MidCube() {
+        return bttmRow_leftComm_MidCube;
+    }
+
+    public boolean getBttmRow_leftComm_RightCone() {
+        return bttmRow_leftComm_RightCone;
+    }
+
+    public boolean getBttmRow_midComm_LeftCone() {
+        return bttmRow_midComm_LeftCone;
+    }
+
+    public boolean getBttmRow_midComm_MidCube() {
+        return bttmRow_midComm_MidCube;
+    }
+
+    public boolean getBttmRow_midComm_RightCone() {
+        return bttmRow_midComm_RightCone;
+    }
+
+    public boolean getBttmRow_rightComm_LeftCone() {
+        return bttmRow_rightComm_LeftCone;
+    }
+
+    public boolean getBttmRow_rightComm_MidCube() {
+        return bttmRow_rightComm_MidCube;
+    }
+
+    public boolean getBttmRow_rightComm_RightCone() {
+        return bttmRow_rightComm_RightCone;
+    }
+
+    public boolean getMidRow_leftComm_LeftCone() {
+        return midRow_leftComm_LeftCone;
+    }
+
+    public boolean getMidRow_leftComm_MidCube() {
+        return midRow_leftComm_MidCube;
+    }
+
+    public boolean getMidRow_leftComm_RightCone() {
+        return midRow_leftComm_RightCone;
+    }
+
+    public boolean getMidRow_midComm_LeftCone() {
+        return midRow_midComm_LeftCone;
+    }
+
+    public boolean getMidRow_midComm_MidCube() {
+        return midRow_midComm_MidCube;
+    }
+
+    public boolean getMidRow_midComm_RightCone() {
+        return midRow_midComm_RightCone;
+    }
+
+    public boolean getMidRow_rightComm_LeftCone() {
+        return midRow_rightComm_LeftCone;
+    }
+
+    public boolean getMidRow_rightComm_MidCube() {
+        return midRow_rightComm_MidCube;
+    }
+
+    public boolean getMidRow_rightComm_RightCone() {
+        return midRow_rightComm_RightCone;
+    }
+
+    public boolean getTopRow_leftComm_LeftCone() {
+        return topRow_leftComm_LeftCone;
+    }
+
+    public boolean getTopRow_leftComm_MidCube() {
+        return topRow_leftComm_MidCube;
+    }
+
+    public boolean getTopRow_leftComm_RightCone() {
+        return topRow_leftComm_RightCone;
+    }
+
+    public boolean getTopRow_midComm_LeftCone() {
+        return topRow_midComm_LeftCone;
+    }
+
+    public boolean getTopRow_midComm_MidCube() {
+        return topRow_midComm_MidCube;
+    }
+
+    public boolean getTopRow_midComm_RightCone() {
+        return topRow_midComm_RightCone;
+    }
+
+    public boolean getTopRow_rightComm_LeftCone() {
+        return topRow_rightComm_LeftCone;
+    }
+
+    public boolean getTopRow_rightComm_MidCube() {
+        return topRow_rightComm_MidCube;
+    }
+
+    public boolean getTopRow_rightComm_RightCone() {
+        return topRow_rightComm_RightCone;
+    }
 
 
-//    public ArrayList<Boolean> getGridVals() {
-//        return gridVals;
-//    }
 }
+
--- scoutingapp/src/main/java/org/usfirst/frc/team25/scouting/ui/dataentry/TeleOpFragment.java
+++ scoutingapp/src/main/java/org/usfirst/frc/team25/scouting/ui/dataentry/TeleOpFragment.java
@@ -33,12 +33,22 @@ public class TeleOpFragment extends Fragment implements EntryFragment {
     private RadioGroup robotFoulGroup, robotDockGroup;
     private ButtonIncDecSet coneTopTele, cubeTopTele, coneMidTele, cubeMidTele, coneBttmTele, cubeBttmTele, coneDroppedTele, cubeDroppedTele;
     private CheckBox robotCommitFoul, robotDockAttempt;
-    private CheckBox[] GridButtons;
-    private String[][] gamePieceHomeGrid = {{"Cone","Cube","Cone","Cone","Cube","Cone","Cone","Cube","Cone"}, //
-                                            {"Cone","Cube","Cone","Cone","Cube","Cone","Cone","Cube","Cone"},
-                                            {"Cone","Cube","Cone","Cone","Cube","Cone","Cone","Cube","Cone"}};
 
-    //Still a work in progress
+    private CheckBox topRow_leftComm_LeftCone,topRow_leftComm_MidCube, topRow_leftComm_RightCone,topRow_midComm_LeftCone,topRow_midComm_MidCube, topRow_midComm_RightCone,
+            topRow_rightComm_LeftCone,topRow_rightComm_MidCube, topRow_rightComm_RightCone,midRow_leftComm_LeftCone,midRow_leftComm_MidCube, midRow_leftComm_RightCone,
+            midRow_midComm_LeftCone,midRow_midComm_MidCube, midRow_midComm_RightCone, midRow_rightComm_LeftCone,midRow_rightComm_MidCube, midRow_rightComm_RightCone,
+            bttmRow_leftComm_LeftCone,bttmRow_leftComm_MidCube, bttmRow_leftComm_RightCone,bttmRow_midComm_LeftCone,bttmRow_midComm_MidCube, bttmRow_midComm_RightCone,
+            bttmRow_rightComm_LeftCone,bttmRow_rightComm_MidCube, bttmRow_rightComm_RightCone;
+
+    private CheckBox[] GridButtons = {topRow_leftComm_LeftCone,topRow_leftComm_MidCube, topRow_leftComm_RightCone,topRow_midComm_LeftCone,topRow_midComm_MidCube, topRow_midComm_RightCone,
+            topRow_rightComm_LeftCone,topRow_rightComm_MidCube, topRow_rightComm_RightCone,midRow_leftComm_LeftCone,midRow_leftComm_MidCube, midRow_leftComm_RightCone,
+            midRow_midComm_LeftCone,midRow_midComm_MidCube, midRow_midComm_RightCone, midRow_rightComm_LeftCone,midRow_rightComm_MidCube, midRow_rightComm_RightCone,
+            bttmRow_leftComm_LeftCone,bttmRow_leftComm_MidCube, bttmRow_leftComm_RightCone,bttmRow_midComm_LeftCone,bttmRow_midComm_MidCube, bttmRow_midComm_RightCone,
+            bttmRow_rightComm_LeftCone,bttmRow_rightComm_MidCube, bttmRow_rightComm_RightCone};   //
+//    private int[] gamePieceHomeGrid = {1,2,1,1,2,1,1,2,1,1,2,1,1,2,1,1,2,1,0,0,0,0,0,0,0,0,0};
+//    private int[] bttmRowButtons = {19,20,21,22,23,24,25,26,27};
+
+    //Still a work in progress      CONE = 1 , CUBE = 2 , Unknown  = 0
 
 
     public static TeleOpFragment getInstance(ScoutEntry entry) {
@@ -91,40 +101,68 @@ public class TeleOpFragment extends Fragment implements EntryFragment {
         robotFoulButtons[1] = view.findViewById(R.id.robot_techFoul);
 
 
-        GridButtons = new CheckBox[27];
+//        GridButtons = new CheckBox[27];
+//        //Row 1
+//        GridButtons[0] = view.findViewById(R.id.checkBox1); // Cone
+//        GridButtons[1] = view.findViewById(R.id.checkBox2); // Cube
+//        GridButtons[2] = view.findViewById(R.id.checkBox3); // Cone
+//        GridButtons[3] = view.findViewById(R.id.checkBox4); // Cone
+//        GridButtons[4] = view.findViewById(R.id.checkBox5); // Cube
+//        GridButtons[5] = view.findViewById(R.id.checkBox6); // Cone
+//        GridButtons[6] = view.findViewById(R.id.checkBox7); // Cone
+//        GridButtons[7] = view.findViewById(R.id.checkBox8); // Cube
+//        GridButtons[8] = view.findViewById(R.id.checkBox9); // Cone
+//        //Row 2
+//        GridButtons[9] = view.findViewById(R.id.checkBox10);
+//        GridButtons[10] = view.findViewById(R.id.checkBox11);
+//        GridButtons[11] = view.findViewById(R.id.checkBox12);
+//        GridButtons[12] = view.findViewById(R.id.checkBox13);
+//        GridButtons[13] = view.findViewById(R.id.checkBox14);
+//        GridButtons[14] = view.findViewById(R.id.checkBox15);
+//        GridButtons[15] = view.findViewById(R.id.checkBox16);
+//        GridButtons[16] = view.findViewById(R.id.checkBox17);
+//        GridButtons[17] = view.findViewById(R.id.checkBox18);
+//        //Row 3
+//        GridButtons[18] = view.findViewById(R.id.checkBox19);
+//        GridButtons[19] = view.findViewById(R.id.checkBox20);
+//        GridButtons[20] = view.findViewById(R.id.checkBox21);
+//        GridButtons[21] = view.findViewById(R.id.checkBox22);
+//        GridButtons[22] = view.findViewById(R.id.checkBox23);
+//        GridButtons[23] = view.findViewById(R.id.checkBox24);
+//        GridButtons[24] = view.findViewById(R.id.checkBox25);
+//        GridButtons[25] = view.findViewById(R.id.checkBox26);
+//        GridButtons[26] = view.findViewById(R.id.checkBox27);
+
         //Row 1
-        GridButtons[0] = view.findViewById(R.id.checkBox1); // Cone
-        GridButtons[1] = view.findViewById(R.id.checkBox2); // Cube
-        GridButtons[2] = view.findViewById(R.id.checkBox3); // Cone
-        GridButtons[3] = view.findViewById(R.id.checkBox4); // Cone
-        GridButtons[4] = view.findViewById(R.id.checkBox5); // Cube
-        GridButtons[5] = view.findViewById(R.id.checkBox6); // Cone
-        GridButtons[6] = view.findViewById(R.id.checkBox7); // Cone
-        GridButtons[7] = view.findViewById(R.id.checkBox8); // Cube
-        GridButtons[8] = view.findViewById(R.id.checkBox9); // Cone
+        topRow_leftComm_LeftCone = view.findViewById(R.id.checkBox1); // Cone
+        topRow_leftComm_MidCube = view.findViewById(R.id.checkBox2); // Cube
+        topRow_leftComm_RightCone = view.findViewById(R.id.checkBox3); // Cone
+        topRow_midComm_LeftCone = view.findViewById(R.id.checkBox4); // Cone
+        topRow_midComm_MidCube = view.findViewById(R.id.checkBox5); // Cube
+        topRow_midComm_RightCone = view.findViewById(R.id.checkBox6); // Cone
+        topRow_rightComm_LeftCone = view.findViewById(R.id.checkBox7); // Cone
+        topRow_rightComm_MidCube = view.findViewById(R.id.checkBox8); // Cube
+        topRow_rightComm_RightCone = view.findViewById(R.id.checkBox9); // Cone
         //Row 2
-        GridButtons[9] = view.findViewById(R.id.checkBox10);
-        GridButtons[10] = view.findViewById(R.id.checkBox11);
-        GridButtons[11] = view.findViewById(R.id.checkBox12);
-        GridButtons[12] = view.findViewById(R.id.checkBox13);
-        GridButtons[13] = view.findViewById(R.id.checkBox14);
-        GridButtons[14] = view.findViewById(R.id.checkBox15);
-        GridButtons[15] = view.findViewById(R.id.checkBox16);
-        GridButtons[16] = view.findViewById(R.id.checkBox17);
-        GridButtons[17] = view.findViewById(R.id.checkBox18);
+        midRow_leftComm_LeftCone = view.findViewById(R.id.checkBox10); // Cone
+        midRow_leftComm_MidCube = view.findViewById(R.id.checkBox11); // Cube
+        midRow_leftComm_RightCone = view.findViewById(R.id.checkBox12); // Cone
+        midRow_midComm_LeftCone = view.findViewById(R.id.checkBox13); // Cone
+        midRow_midComm_MidCube = view.findViewById(R.id.checkBox14); // Cube
+        midRow_midComm_RightCone = view.findViewById(R.id.checkBox15); // Cone
+        midRow_rightComm_LeftCone = view.findViewById(R.id.checkBox16); // Cone
+        midRow_rightComm_MidCube = view.findViewById(R.id.checkBox17); // Cube
+        midRow_rightComm_RightCone = view.findViewById(R.id.checkBox18); // Cone
         //Row 3
-        GridButtons[18] = view.findViewById(R.id.checkBox19);
-        GridButtons[19] = view.findViewById(R.id.checkBox20);
-        GridButtons[20] = view.findViewById(R.id.checkBox21);
-        GridButtons[21] = view.findViewById(R.id.checkBox22);
-        GridButtons[22] = view.findViewById(R.id.checkBox23);
-        GridButtons[23] = view.findViewById(R.id.checkBox24);
-        GridButtons[24] = view.findViewById(R.id.checkBox25);
-        GridButtons[25] = view.findViewById(R.id.checkBox26);
-        GridButtons[26] = view.findViewById(R.id.checkBox27);
-
-
-
+        bttmRow_leftComm_LeftCone = view.findViewById(R.id.checkBox19); // Cone
+        bttmRow_leftComm_MidCube = view.findViewById(R.id.checkBox20); // Cube
+        bttmRow_leftComm_RightCone = view.findViewById(R.id.checkBox21); // Cone
+        bttmRow_midComm_LeftCone = view.findViewById(R.id.checkBox22); // Cone
+        bttmRow_midComm_MidCube = view.findViewById(R.id.checkBox23); // Cube
+        bttmRow_midComm_RightCone = view.findViewById(R.id.checkBox24); // Cone
+        bttmRow_rightComm_LeftCone = view.findViewById(R.id.checkBox25); // Cone
+        bttmRow_rightComm_MidCube = view.findViewById(R.id.checkBox26); // Cube
+        bttmRow_rightComm_RightCone = view.findViewById(R.id.checkBox27); // Cone
 
 
 
@@ -207,6 +245,36 @@ public class TeleOpFragment extends Fragment implements EntryFragment {
             cubeBttmTele.setValue(tele.getCubeBttm());
             coneDroppedTele.setValue(tele.getConeDropped());
             cubeDroppedTele.setValue(tele.getCubeDropped());
+            topRow_leftComm_LeftCone.setChecked(tele.getTopRow_leftComm_LeftCone());
+            topRow_leftComm_MidCube.setChecked(tele.getTopRow_leftComm_MidCube());
+            topRow_leftComm_RightCone.setChecked(tele.getTopRow_leftComm_RightCone());
+            topRow_midComm_LeftCone.setChecked(tele.getTopRow_midComm_LeftCone());
+            topRow_midComm_MidCube.setChecked(tele.getTopRow_midComm_MidCube());
+            topRow_midComm_RightCone.setChecked(tele.getTopRow_midComm_RightCone());
+            topRow_rightComm_LeftCone.setChecked(tele.getTopRow_rightComm_LeftCone());
+            topRow_rightComm_MidCube.setChecked(tele.getTopRow_rightComm_MidCube());
+            topRow_rightComm_RightCone.setChecked(tele.getTopRow_rightComm_RightCone());
+
+            midRow_leftComm_LeftCone.setChecked(tele.getMidRow_leftComm_LeftCone());
+            midRow_leftComm_MidCube.setChecked(tele.getMidRow_leftComm_MidCube());
+            midRow_leftComm_RightCone.setChecked(tele.getMidRow_leftComm_RightCone());
+            midRow_midComm_LeftCone.setChecked(tele.getMidRow_midComm_LeftCone());
+            midRow_midComm_MidCube.setChecked(tele.getMidRow_midComm_MidCube());
+            midRow_midComm_RightCone.setChecked(tele.getMidRow_midComm_RightCone());
+            midRow_rightComm_LeftCone.setChecked(tele.getMidRow_rightComm_LeftCone());
+            midRow_rightComm_MidCube.setChecked(tele.getMidRow_rightComm_MidCube());
+            midRow_rightComm_RightCone.setChecked(tele.getMidRow_rightComm_RightCone());
+
+            bttmRow_leftComm_LeftCone.setChecked(tele.getBttmRow_leftComm_LeftCone());
+            bttmRow_leftComm_MidCube.setChecked(tele.getBttmRow_leftComm_MidCube());
+            bttmRow_leftComm_RightCone.setChecked(tele.getBttmRow_leftComm_RightCone());
+            bttmRow_midComm_LeftCone.setChecked(tele.getBttmRow_midComm_LeftCone());
+            bttmRow_midComm_MidCube.setChecked(tele.getBttmRow_midComm_MidCube());
+            bttmRow_midComm_RightCone.setChecked(tele.getBttmRow_midComm_RightCone());
+            bttmRow_rightComm_LeftCone.setChecked(tele.getBttmRow_rightComm_LeftCone());
+            bttmRow_rightComm_MidCube.setChecked(tele.getBttmRow_rightComm_MidCube());
+            bttmRow_rightComm_RightCone.setChecked(tele.getBttmRow_rightComm_RightCone());
+
 
             for (RadioButton button : robotDockButtons) {
                 if (button.getText().toString().equals(tele.getDockStatus())) {
@@ -220,9 +288,8 @@ public class TeleOpFragment extends Fragment implements EntryFragment {
                 }
             }
 
-//            HashMap<CheckBox, Boolean> GridValuesAutoPopulate = new HashMap<CheckBox, Boolean>();
-//            //GridValuesAutoPopulate = tele.getGridVals();
-//
+
+//            HashMap<CheckBox, Boolean> GridValuesAutoPopulate = tele.getGridVals();
 //            for(CheckBox checkBoxValue: GridButtons){
 //                checkBoxValue.setChecked(GridValuesAutoPopulate.get(checkBoxValue));
 //                //checkBoxValue.set()
@@ -238,13 +305,13 @@ public class TeleOpFragment extends Fragment implements EntryFragment {
     @Override
     public void saveState() {
 
-//       // HashMap<CheckBox, Boolean> GridValues = new HashMap<CheckBox, Boolean>();
+//       HashMap<CheckBox, Boolean> GridValues = new HashMap<CheckBox, Boolean>();
 //
-//        boolean[][] GridValues = new boolean[3][9];
+//        //boolean[][] GridValues = new boolean[3][9];
 //        //ArrayList<Boolean> GridValues = new ArrayList<Boolean>();
 //        for(CheckBox checkBoxValue: GridButtons){
-//            //GridValues.put(checkBoxValue, checkBoxValue.isChecked());
-//            GridValues.add(checkBoxValue.isChecked());
+//            GridValues.put(checkBoxValue, checkBoxValue.isChecked());
+//            //GridValues.add(checkBoxValue.isChecked());
 //        }
 
         String dockStatus = "";
@@ -267,9 +334,18 @@ public class TeleOpFragment extends Fragment implements EntryFragment {
 
         entry.setTeleOp(new TeleOp(coneTopTele.getValue(),cubeTopTele.getValue(),coneMidTele.getValue(),
                 cubeMidTele.getValue(), coneBttmTele.getValue(), cubeBttmTele.getValue(), coneDroppedTele.getValue(),
-                cubeDroppedTele.getValue(),robotDockAttempt.isChecked(),dockStatus, robotCommitFoul.isChecked(),
-                foulType
-                //GridValues
+                cubeDroppedTele.getValue(),robotDockAttempt.isChecked(),dockStatus, robotCommitFoul.isChecked(),foulType,
+                topRow_leftComm_LeftCone.isChecked(),topRow_leftComm_MidCube.isChecked(), topRow_leftComm_RightCone.isChecked(),topRow_midComm_LeftCone.isChecked(),
+                topRow_midComm_MidCube.isChecked(), topRow_midComm_RightCone.isChecked(), topRow_rightComm_LeftCone.isChecked(),topRow_rightComm_MidCube.isChecked(),
+                topRow_rightComm_RightCone.isChecked(),
+
+                midRow_leftComm_LeftCone.isChecked(),midRow_leftComm_MidCube.isChecked(), midRow_leftComm_RightCone.isChecked(),
+                midRow_midComm_LeftCone.isChecked(),midRow_midComm_MidCube.isChecked(), midRow_midComm_RightCone.isChecked(),
+                midRow_rightComm_LeftCone.isChecked(), midRow_rightComm_MidCube.isChecked(), midRow_rightComm_RightCone.isChecked(),
+
+                bttmRow_leftComm_LeftCone.isChecked(),bttmRow_leftComm_MidCube.isChecked(), bttmRow_leftComm_RightCone.isChecked(),
+                bttmRow_midComm_LeftCone.isChecked(),bttmRow_midComm_MidCube.isChecked(), bttmRow_midComm_RightCone.isChecked(),
+                bttmRow_rightComm_LeftCone.isChecked(),bttmRow_rightComm_MidCube.isChecked(), bttmRow_rightComm_RightCone.isChecked()
         ));
 
 
