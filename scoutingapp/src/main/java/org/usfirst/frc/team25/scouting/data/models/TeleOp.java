3004fb2c85a39146bc3a03f87c320df7e41092c0
Sameer152010 <96846309+Sameer152010@users.noreply.github.com>
2024-01-27 12:00:38 -0500

    1/27/24 Teleop Complete, Build Sucessful

--- scoutingapp/src/main/java/org/usfirst/frc/team25/scouting/data/models/TeleOp.java
+++ scoutingapp/src/main/java/org/usfirst/frc/team25/scouting/data/models/TeleOp.java
@@ -7,131 +7,40 @@ package org.usfirst.frc.team25.scouting.data.models;
  */
 public class TeleOp {
 
-    private int cargoShipHatches;
-    private int rocketLevelOneHatches;
-    private int rocketLevelTwoHatches;
-    private int rocketLevelThreeHatches;
-    private int cargoShipCargo;
-    private int rocketLevelOneCargo;
-    private int rocketLevelTwoCargo;
-    private int rocketLevelThreeCargo;
+    private int notesPickedGround;
+    private int notesPickedHps;
+    private int ampNotesScoredTelop;
+    private int speakerNotesScoredTeleop;
+    private int ampSpeakerNotesScored;
+    private int notesDroppedTeleop;
 
-    private int hatchesDropped;
-    private int cargoDropped;
-
-    private boolean attemptHabClimb;
-    private int attemptHabClimbLevel;
-    private boolean successHabClimb;
-    private int successHabClimbLevel;
-    private boolean climbAssistedByPartner;
-    private int assistingClimbTeamNum;
-    private int numPartnerClimbAssists;
-    private int partnerClimbAssistStartLevel;
-    private int partnerClimbAssistEndLevel;
-
-    public TeleOp(int cargoShipHatches, int rocketLevelOneHatches, int rocketLevelTwoHatches,
-                  int rocketLevelThreeHatches, int cargoShipCargo, int rocketLevelOneCargo,
-                  int rocketLevelTwoCargo, int rocketLevelThreeCargo, int hatchesDropped,
-                  int cargoDropped, boolean attemptHabClimb, int attemptHabClimbLevel,
-                  boolean successHabClimb, int successHabClimbLevel,
-                  boolean climbAssistedByPartner, int assistingClimbTeamNum,
-                  int numPartnerClimbAssists, int partnerClimbAssistEndLevel,
-                  int partnerClimbAssistStartLevel) {
-        this.cargoShipHatches = cargoShipHatches;
-        this.rocketLevelOneHatches = rocketLevelOneHatches;
-        this.rocketLevelTwoHatches = rocketLevelTwoHatches;
-        this.rocketLevelThreeHatches = rocketLevelThreeHatches;
-        this.cargoShipCargo = cargoShipCargo;
-        this.rocketLevelOneCargo = rocketLevelOneCargo;
-        this.rocketLevelTwoCargo = rocketLevelTwoCargo;
-        this.rocketLevelThreeCargo = rocketLevelThreeCargo;
-        this.hatchesDropped = hatchesDropped;
-        this.cargoDropped = cargoDropped;
-        this.attemptHabClimb = attemptHabClimb;
-        this.attemptHabClimbLevel = attemptHabClimbLevel;
-        this.successHabClimb = successHabClimb;
-        this.successHabClimbLevel = successHabClimbLevel;
-        this.climbAssistedByPartner = climbAssistedByPartner;
-        this.assistingClimbTeamNum = assistingClimbTeamNum;
-        this.numPartnerClimbAssists = numPartnerClimbAssists;
-        this.partnerClimbAssistEndLevel = partnerClimbAssistEndLevel;
-        this.partnerClimbAssistStartLevel = partnerClimbAssistStartLevel;
+    public TeleOp(int notesPickedGround, int notesPickedHps, int ampNotesScoredTelop, int speakerNotesScoredTeleop, int ampSpeakerNotesScored, int notesDroppedTeleop) {
+        this.notesPickedGround = notesPickedGround;
+        this.notesPickedHps = notesPickedHps;
+        this.ampNotesScoredTelop = ampNotesScoredTelop;
+        this.speakerNotesScoredTeleop = speakerNotesScoredTeleop;
+        this.ampSpeakerNotesScored = ampSpeakerNotesScored;
+        this.notesDroppedTeleop = notesDroppedTeleop;
     }
 
 
-    public int getCargoShipHatches() {
-        return cargoShipHatches;
+    public int getNotesPickedGround() {
+        return notesPickedGround;
     }
 
-    public int getRocketLevelOneHatches() {
-        return rocketLevelOneHatches;
+    public int getNotesPickedHps() {
+        return notesPickedHps;
     }
 
-    public int getRocketLevelTwoHatches() {
-        return rocketLevelTwoHatches;
+    public int getAmpNotesScoredTelop() {
+        return ampNotesScoredTelop;
     }
 
-    public int getRocketLevelThreeHatches() {
-        return rocketLevelThreeHatches;
-    }
+    public int getSpeakerNotesScoredTeleop() {return speakerNotesScoredTeleop;}
 
-    public int getCargoShipCargo() {
-        return cargoShipCargo;
-    }
+    public int getAmpSpeakerNotesScored() {return ampSpeakerNotesScored;}
 
-    public int getRocketLevelOneCargo() {
-        return rocketLevelOneCargo;
-    }
-
-    public int getRocketLevelTwoCargo() {
-        return rocketLevelTwoCargo;
-    }
-
-    public int getRocketLevelThreeCargo() {
-        return rocketLevelThreeCargo;
-    }
-
-    public int getHatchesDropped() {
-        return hatchesDropped;
-    }
-
-    public int getCargoDropped() {
-        return cargoDropped;
-    }
-
-    public boolean isAttemptHabClimb() {
-        return attemptHabClimb;
-    }
-
-    public int getAttemptHabClimbLevel() {
-        return attemptHabClimbLevel;
-    }
-
-    public boolean isSuccessHabClimb() {
-        return successHabClimb;
-    }
-
-    public int getSuccessHabClimbLevel() {
-        return successHabClimbLevel;
-    }
-
-    public boolean isClimbAssistedByPartner() {
-        return climbAssistedByPartner;
-    }
-
-    public int getAssistingClimbTeamNum() {
-        return assistingClimbTeamNum;
-    }
-
-    public int getNumPartnerClimbAssists() {
-        return numPartnerClimbAssists;
-    }
-
-    public int getPartnerClimbAssistEndLevel() {
-        return partnerClimbAssistEndLevel;
-    }
-
-    public int getPartnerClimbAssistStartLevel() {
-        return partnerClimbAssistStartLevel;
+    public int getNotesDroppedTeleop() {
+        return notesDroppedTeleop;
     }
 }
